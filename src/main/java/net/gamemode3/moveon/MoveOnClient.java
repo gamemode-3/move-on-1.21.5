e5048946021581fdd25d7b11ecffb70dd1015f77
gamemode-3 <simon@kriele.de>
2025-06-02 19:27:16 +0200

    Initial commit

---
+++ src/main/java/net/gamemode3/moveon/MoveOnClient.java
@@ -0,0 +1,10 @@
+package net.gamemode3.moveon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoveOnClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gamemode3/moveon/MoveOnDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gamemode3.moveon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoveOnDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
